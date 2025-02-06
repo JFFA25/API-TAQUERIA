5b4ba31ea962e3976c879bd8491b0c6f9d1fe704
JFFA25 <230417@utxicotepec.edu.mx>
2025-02-05 18:48:45 -0600

    Actualizando
    Actualizando datos para la conexion a la base de datos

--- src/database.js
+++ src/database.js
@@ -3,7 +3,7 @@ import { mongoose } from "mongoose";
 
 
 mongoose.connect(process.env.CONNECTION_STRING_DB)
-  .then(db => console.log(`Database Connected`))
+  .then(db => console.log(`Base de datos conectada`))
   .catch(err => console.error(err));
 
 export default mongoose;
--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@ import app from "./app.js";
 import database from './database.js'
 
 app.listen(process.env.PORT,() =>{
-    console.log(`Server on port: ` + process.env.PORT);
+    console.log(`Servidor en el puerto: ` + process.env.PORT);
 })
